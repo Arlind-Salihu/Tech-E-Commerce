54b99c5fd1349d509c9bfb1cb3545bd76f8f67e6
Arlind-Salihu <as47047@ubt-uni.net>
2022-01-06 23:10:58 +0100

    Fillimi i projektit per Tech E-Commerce, komponenta e pare(Test)

---
+++ tech-e-commerce/src/main.js
@@ -0,0 +1,12 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+
+Vue.config.productionTip = false;
+
+Vue.component("alert", require('./components/Alert.vue').default);
+
+new Vue({
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
