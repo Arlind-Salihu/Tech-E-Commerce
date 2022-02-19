473e6a7e673aa6dfe4d2bd46b843318375349510
Arlind-Salihu <as47047@ubt-uni.net>
2022-02-19 20:03:29 +0100

    Permiresime per funksionalizim te projektit

--- tech-e-commerce/src/views/Home.vue
+++ tech-e-commerce/src/views/Home.vue
@@ -5,14 +5,14 @@
       <div class="container h-100">
         <div class="row p-5 h-100 justify-content-center align-items-center">
           <div class="col-md-6">
-            <img src="/img/svg/product.svg" alt="" width="400">
+            <img src="../assets/product.svg" alt="" width="400">
           </div>
           <div class="col-md-6">
             <h3>Learn More About us</h3>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur quaerat voluptate iusto? Nihil voluptas vitae veritatis magnam reprehenderit, reiciendis ex? Libero quasi natus veniam voluptatum, voluptas exercitationem ratione consectetur et!
             </p>
-             <button class="btn custom-btn btn-outline-primary mr-4"><router-link to="/checkout" class="nav-link"></router-link>Checkout Products</button>
+             <button class="button-29" role="button"><a href="/checkout" id="a">Checkout Products</a></button>
           </div>
         </div>
       </div>
@@ -39,5 +39,56 @@ export default {
 <style>
 .home{
   background: #f2f2f2;
+}
+
+
+/* CSS */
+#a{
+  text-decoration: none;
+  color:white;
+}
+.button-29 {
+  align-items: center;
+  appearance: none;
+  background-image: radial-gradient(100% 100% at 100% 0, #5adaff 0, #5468ff 100%);
+  border: 0;
+  border-radius: 6px;
+  box-shadow: rgba(45, 35, 66, .4) 0 2px 4px,rgba(45, 35, 66, .3) 0 7px 13px -3px,rgba(58, 65, 111, .5) 0 -3px 0 inset;
+  box-sizing: border-box;
+  color: #fff;
+  cursor: pointer;
+  display: inline-flex;
+  font-family: "JetBrains Mono",monospace;
+  height: 48px;
+  justify-content: center;
+  line-height: 1;
+  list-style: none;
+  overflow: hidden;
+  padding-left: 16px;
+  padding-right: 16px;
+  position: relative;
+  text-align: left;
+  text-decoration: none;
+  transition: box-shadow .15s,transform .15s;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  white-space: nowrap;
+  will-change: box-shadow,transform;
+  font-size: 18px;
+}
+
+.button-29:focus {
+  box-shadow: #3c4fe0 0 0 0 1.5px inset, rgba(45, 35, 66, .4) 0 2px 4px, rgba(45, 35, 66, .3) 0 7px 13px -3px, #3c4fe0 0 -3px 0 inset;
+}
+
+.button-29:hover {
+  box-shadow: rgba(45, 35, 66, .4) 0 4px 8px, rgba(45, 35, 66, .3) 0 7px 13px -3px, #3c4fe0 0 -3px 0 inset;
+  transform: translateY(-2px);
+}
+
+.button-29:active {
+  box-shadow: #3c4fe0 0 3px 7px inset;
+  transform: translateY(2px);
 }
 </style>
